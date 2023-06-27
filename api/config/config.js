c8996dc9faee98d54c381c7b13b2b345d5eeea8e
luizm-souza <luizmatheus.csouza@gmail.com>
2023-06-26 23:14:17 -0300

    Testando config.js

--- api/config/config.js
+++ api/config/config.js
@@ -1,13 +1,24 @@
 require('dotenv').config(); // Carrega as variáveis de ambiente do arquivo .env
 
 module.exports = {
-  production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'mysql'
-  }
-};
+    development: {
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: 'mysql'
+    },
+    test: {
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: 'mysql'
+    },
+    production: {
+      use_env_variable: 'DATABASE_URL',
+      dialect: 'mysql'
+    }
+  };
