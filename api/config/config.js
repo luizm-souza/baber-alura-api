3e78778f6c5c7ca19888c9e8dbaf8e0e0201ea1f
luizm-souza <luizmatheus.csouza@gmail.com>
2023-06-26 23:19:30 -0300

    Testando config.json

--- api/config/config.js
+++ api/config/config.js
@@ -9,15 +9,12 @@ module.exports = {
       port: process.env.DB_PORT,
       dialect: 'mysql'
     },
-    test: {
-      username: process.env.DB_USERNAME,
+    production: {
+        username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
-      dialect: 'mysql'
-    },
-    production: {
       use_env_variable: 'DATABASE_URL',
       dialect: 'mysql'
     }
